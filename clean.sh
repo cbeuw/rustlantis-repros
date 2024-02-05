6859106014682fed867b2349e36a9a3bc3d00215
Andy Wang <cbeuw.andy@gmail.com>
2024-02-05 22:59:57 +0000

    Add timeout to clean

--- clean.sh
+++ clean.sh
@@ -5,5 +5,5 @@ set -eu
 TRIPLE=$(rustc -vV | sed -n 's|host: ||p')
 cd $TRIPLE
 mkdir -p solved
-ls repros-*/*.rs | xargs -I{} -P4 sh -c 'echo {}; ~/rustlantis/target/release/difftest {} 2> /dev/null || exit;  echo "{} solved"; mv {} solved'
+ls repros-*/*.rs | xargs -I{} -P4 sh -c 'echo {}; timeout 20 difftest {} 2> /dev/null || exit;  echo "{} solved"; mv {} solved'
 
